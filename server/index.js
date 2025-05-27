3930be9e0ca9bb5e1e92cb1d2aba47961c2372e3
snarfisoka <snarfisoka@gmail.com>
2025-05-27 20:10:35 +0800

    updated host in api routes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-    .connect(process.env.MONGO_URL, {
+    .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
